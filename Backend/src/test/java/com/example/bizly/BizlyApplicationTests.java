95dc62254115ca6d1affdec6bbc81729cafaba71
Esteban <estebanarroyave663@gmail.com>
2026-05-25 14:12:10 -0500

    Inicializacion del proyecto

---
+++ Backend/src/test/java/com/example/bizly/BizlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bizly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
