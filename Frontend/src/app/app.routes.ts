f2e857bb25fe972a2614d46f88f19272df5857a7
Mateo <mq202041@gmail.com>
2026-05-26 12:04:41 -0500

    creación de login y conifguración de rutas entre loging y productos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { ProductosComponent } from './productos/app/productos/productos';
 import { Routes } from '@angular/router';
-
+import { LoginComponent } from './auth/components/login/login.component';
 export const routes: Routes = [
-  // Ruta por defecto (redirige a productos al entrar a la app)
-  { path: '', redirectTo: 'productos', pathMatch: 'full' },
-  
-  // Ruta de la página de productos
+
+  // Primera pantalla
+  { path: '', component: LoginComponent },
+
+  // Productos
   { path: 'productos', component: ProductosComponent },
-  
-  // Ruta comodín (por si escriben una URL que no existe)
-  { path: '**', redirectTo: 'productos' }
+
+  // Ruta comodín
+  { path: '**', redirectTo: '' }
+
 ];
